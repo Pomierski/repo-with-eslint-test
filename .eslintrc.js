e93a2366e8514661a202121fb1a8805243d625ca
Piotr Pomierski <pomierskipiotr@gmail.com>
2022-11-28 08:32:10 +0100

    eslint.rc

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "./node_modules/eslint-test/.eslintrc.js",
+    "plugins": ["prefer-arrow"],
+    "rules": {
+        "prefer-arrow/prefer-arrow-functions": ["warn"]
+    }
+}
